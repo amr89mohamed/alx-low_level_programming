a623d55852e2cc9d672cd5ff09198fd4d4ad4304
amr89mohamed <amr89mohamed@gmail.com>
2023-08-12 06:04:01 -0700

    add a and an

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int b;
 long int c;
 long long int m;
 float k;
-printf("size of char: %lu \n",(unsigned long)sizeof (i));
-printf("size of int: %lu \n",(unsigned long)sizeof (b));
-printf("size of long int: %lu \n",(unsigned long)sizeof (c));
-printf("size of long long int: %lu \n",(unsigned long)sizeof (m));
-printf("size of float: %lu \n",(unsigned long)sizeof (k));
+printf("size of a char: %lu \n",(unsigned long)sizeof (i));
+printf("size of an int: %lu \n",(unsigned long)sizeof (b));
+printf("size of a long int: %lu \n",(unsigned long)sizeof (c));
+printf("size of a long long int: %lu \n",(unsigned long)sizeof (m));
+printf("size of a float: %lu \n",(unsigned long)sizeof (k));
 return (0);  
 }
