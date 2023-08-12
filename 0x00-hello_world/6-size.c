2b0430afb2b30fcabcd6f1d13a38843f3ef89e04
amr89mohamed <amr89mohamed@gmail.com>
2023-08-12 06:24:03 -0700

    run betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int b;
 long int c;
 long long int m;
 float k;
- printf("size of a char: %lu byte(s)\n",(unsigned long)sizeof (i));
-printf("size of an int: %lu byte(s)\n",(unsigned long)sizeof (b));
- printf("size of a long int: %lu byte(s)\n",(unsigned long)sizeof (c));
-printf("size of a long long int: %lu byte(s)\n",(unsigned long)sizeof (m));
-printf("size of a float: %lu byte(s)\n",(unsigned long)sizeof (k));
-return (0);  
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(m));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(k));
+return (0);
 }
