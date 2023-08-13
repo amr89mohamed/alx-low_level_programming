c1d168df199a926aeef2adadc0f4f8878c503392
amr89mohamed <amr89mohamed@gmail.com>
2023-08-13 16:17:03 -0700

    add void

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 /*
- *thid is the mainfuncation */int main()
+ *thid is the mainfuncation */int main(void)
 {
 char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
-{
 putchar (ch);
-}
 return (0);
 }
 
