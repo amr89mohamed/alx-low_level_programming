6eb145dc30b735077bc609efcb13240fe0e4e83b
amr89mohamed <amr89mohamed@gmail.com>
2023-08-14 16:06:50 -0700

    status

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,11 @@
  */
 int main(void)
 {
-char i;
-int b;
-long int c;
-long long int m;
-float k;
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(i));
-printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(m));
-printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(k));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
+perror("status");
 return (0);
 }
