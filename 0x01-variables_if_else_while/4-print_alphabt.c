e894c26d8a0a866afc46e6612caf088dfc006aae
amr89mohamed <amr89mohamed@gmail.com>
2023-08-14 05:46:06 -0700

    use if

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ *this the main funcation*/ int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch != 'q' && ch != 'e')
+putchar (ch);
+}
+putchar ('\n');
+return (0);
+}
