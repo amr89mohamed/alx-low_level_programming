b72dab106bb68fe09ef7e739b4df20366e3abec2
amr89mohamed <amr89mohamed@gmail.com>
2023-08-13 17:41:39 -0700

    remove space

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,12 +4,12 @@
 {
 {
 char ch;
-for ( ch = 'a' ; ch <= 'z' ; ++ch)
+for (ch = 'a' ; ch <= 'z' ; ++ch)
 putchar (ch);
 }
 {
 char ch1;
-for ( ch1 = 'A' ; ch1 <= 'Z' ; ch1++)
+for (ch1 = 'A' ; ch1 <= 'Z' ; ch1++)
 putchar (ch1);
 }
 putchar ('\n');
