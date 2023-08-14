e0d3e29b1cae60aa797f8e470028c6234753ed72
amr89mohamed <amr89mohamed@gmail.com>
2023-08-14 16:12:37 -0700

    i dont know what should i do

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,16 @@
  */
 int main(void)
 {
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
-printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
-printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
-printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
-printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
+char c;
+int i;
+long int li;
+long long int lli;
+float f;
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 perror("status");
 return (0);
 }
