a569957db60f8efc6356b25582c54b927af9eba6
amr89mohamed <amr89mohamed@gmail.com>
2023-08-14 16:53:21 -0700

    betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int i;
 long int li;
 long long int lli;
 float f;
-printf("size of a char:  %lu byte(s)\n",(unsigned long)sizeof(ch));
+printf("size of a char:  %lu byte(s)\n", (unsigned long)sizeof(ch));
 printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
 printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
- printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f)); 
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
