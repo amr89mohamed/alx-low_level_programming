f17db62067e3999c8624c21cbbd55fc687c9c290
amr89mohamed <amr89mohamed@gmail.com>
2023-08-14 15:29:09 -0700

    add if

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,11 +3,14 @@
  *this the main funcation, main is good*/ int main(void)
 {
 int d;
-for (d = '1' ; d <= '9' ; d++)
+for (d = '0' ; d <= '9' ; d++)
 {
 putchar(d);
+{
+if (d != '9')
 putchar(',');
 putchar(' ');
+}
 }
 putchar('\n');
 return (0);
