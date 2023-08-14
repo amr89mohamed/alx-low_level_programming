d46f920110d0caa5762dc7c22d0afb8c4482974f
amr89mohamed <amr89mohamed@gmail.com>
2023-08-13 17:35:49 -0700

    aA

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+*thid is the mainfuncation */int main(void)
+{
+char ch;
+char ch1;
+{
+for (char ch = 'a' ; ch <= 'z' ; ++ch)
+putchar (ch);
+}
+{
+for (char ch1 = 'A' ; ch1 <= 'Z' ; ch1++)
+putchar (ch1);
+}
+putchar ('\n');
+return (0);
+}
