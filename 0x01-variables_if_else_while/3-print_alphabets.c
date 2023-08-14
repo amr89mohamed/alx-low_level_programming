c7f7175ebeb9f090e1d79ecd026e16c13eee5f70
amr89mohamed <amr89mohamed@gmail.com>
2023-08-13 17:40:24 -0700

    remove char

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,12 +4,12 @@
 {
 {
 char ch;
-for (char ch = 'a' ; ch <= 'z' ; ++ch)
+for ( ch = 'a' ; ch <= 'z' ; ++ch)
 putchar (ch);
 }
 {
 char ch1;
-for (char ch1 = 'A' ; ch1 <= 'Z' ; ch1++)
+for ( ch1 = 'A' ; ch1 <= 'Z' ; ch1++)
 putchar (ch1);
 }
 putchar ('\n');
