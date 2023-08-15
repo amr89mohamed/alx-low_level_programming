dbae4d2e0e3485c4fda1a071c409fd6f6c9bca88
amr89mohamed <amr89mohamed@gmail.com>
2023-08-14 18:45:26 -0700

    make it

--- 0x00-hello_world/100-main.c
+++ 0x00-hello_world/100-main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  printf("100-c");
-return (0);
+	printf("Holberton School");
+	return (0);
 }
