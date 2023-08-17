c47a7d0e518c138325c3cb7686c7c9fdc7c66fd2
amr89mohamed <amr89mohamed@gmail.com>
2023-08-16 20:06:26 -0700

    22

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,19 +1,25 @@
 #include "3-lower.h"
 /**
- * main - check the code.
+ * test_islower - Test the _islower function
+ *
+ * @n: Number to pass to _islower function
+ */
+void test_islower(int n)
+{
+	int r;
+
+	r = _islower(n);
+	_putchar(r + '0');
+	_putchar('\n');
+}
+
+/**
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int r;
-
-    r = test_islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+	test_islower('H');
+	return (0);
 }
