0eeb6078d36d5b991de6bd7f3d3bc098410124bc
amr89mohamed <amr89mohamed@gmail.com>
2023-08-16 18:11:26 -0700

    i think all is good

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "mian.h"
+int main(void)
+{
+char c;
+int i;
+while (i>=10)
+{
+for (c = 'a' ; c <= 'z' ; c++)
+_putchar(c);
+}
+_putchar(i);
+i++
+return (0);
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,2 +1,14 @@
 #include "main.h"
-void print_alphabet_x10(void)
+/*
+ *this a print funcation */ void print_alphabet_x10(void)
+{
+char c;
+int i;
+while (i >= 10)
+{
+for (c = 'a' ; c <= 'z' ; c++)
+_putchar(c);
+}
+_putchar(i);
+i++
+}
