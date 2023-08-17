72d458fbff2e9dd1c6ec739814e4f41e0fa173a9
amr89mohamed <amr89mohamed@gmail.com>
2023-08-16 17:38:32 -0700

    110

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,12 @@
 #include "main.h"
 void print_alphabet(void);
-{
   return print_alphabet;
+{
+    char c;
+
+    for (c = 'a'; c <= 'z'; c++)
+    {
+        _putchar(c);
+    }
+    _putchar('\n');
 }
