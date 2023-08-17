ef4b06ce5ecf2b2f0c93a316a3e806a090ecb322
amr89mohamed <amr89mohamed@gmail.com>
2023-08-16 18:28:52 -0700

    chamge the header file

---
+++ 0x02-functions_nested_loops/2-main.h
@@ -0,0 +1,6 @@
+#ifndef 2-MAIN_H
+#define 2-MAIN_H
+void print_alphabet_x10(void);
+int _putchar(char c);
+
+#endif
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "2-main.h"
 /*
  *this a print funcation */ void print_alphabet_x10(void)
 {
