d51e6e0858760118c1d6ec1d725d4d299f99151f
amr89mohamed <amr89mohamed@gmail.com>
2023-08-18 06:07:36 -0700

    add the main funcation

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -5,7 +5,15 @@
   *more headers goes there
   *serves as the starting point for program execution
   *this the funcation decleratio */ void positive_or_negative(int i)
-
+{
+  /* 
+   *this the main funcation */ int main(void)
+{
+int i;
+i = 98;
+positive_or_negative(i);
+return (0);
+}
 {
 if (i > 0)
 {
@@ -18,5 +26,6 @@ printf("%d is zero\n", i);
 else
 {
 printf("%d is negative\n", i);
+}
 }
 }
