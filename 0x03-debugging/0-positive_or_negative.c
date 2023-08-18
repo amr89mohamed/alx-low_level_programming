3ea774be60185c6c9a8e0b649dacb9c115b2433b
amr89mohamed <amr89mohamed@gmail.com>
2023-08-18 06:13:51 -0700

    i want to compile it

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -1,19 +1,10 @@
 #include <time.h>
 #include <stdio.h>
 #include "main.h"
- /*
+/*
   *more headers goes there
   *serves as the starting point for program execution
   *this the funcation decleratio */ void positive_or_negative(int i)
-{
-  /* 
-   *this the main funcation */ int main(void)
-{
-int i;
-i = 98;
-positive_or_negative(i);
-return (0);
-}
 {
 if (i > 0)
 {
@@ -26,6 +17,5 @@ printf("%d is zero\n", i);
 else
 {
 printf("%d is negative\n", i);
-}
 }
 }
