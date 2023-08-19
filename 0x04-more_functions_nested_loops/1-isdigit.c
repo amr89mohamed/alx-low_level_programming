12bef63c4604b8cb009ea334c02f3ee4d27ae063
amr89mohamed <amr89mohamed@gmail.com>
2023-08-19 16:23:29 -0700

    check is the funcation can check c isdigit or not

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <ctype.h>
+/*
+ *this the isdigit funcation which will check if 
+ * c is didgit or not
+ */int _isdigit(int c)
+{
+if (isdigit(c))
+return (1);
+else
+return (0);
+}
