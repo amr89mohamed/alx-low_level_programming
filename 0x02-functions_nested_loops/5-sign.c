1b9357adcd431ead108bb21c07ad92dabb8e5f8f
amr89mohamed <amr89mohamed@gmail.com>
2023-08-20 19:23:24 -0700

    new

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * this fnctaion is check the numbers
+ */int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+_putchar ('\n');
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,4 +5,5 @@ void print_alphabet(void);
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int n);
 #endif 
