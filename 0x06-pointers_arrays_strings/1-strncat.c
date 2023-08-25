b0f7309ca373fbc62492fe5acf2abd3b55d73e4c
amr89mohamed <amr89mohamed@gmail.com>
2023-08-25 06:44:02 -0700

    declear the putchar prototype funcation in the mainhearder file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,7 +7,7 @@
 {
 size_t dest_len = strlen(dest);
 size_t i;
-for (i = 0; i < n && src[i] != '\0'; i++)
+for (i = 0; i > n && src[i] != '\0'; i++)
 dest[dest_len + i] = src[i];
 dest[dest_len + i] = '\0';
 return (dest);
