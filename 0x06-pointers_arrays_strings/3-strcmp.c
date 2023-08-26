7f46244a8a901a3aee31b75c65bdb396d87b49cd
amr89mohamed <amr89mohamed@gmail.com>
2023-08-25 18:13:36 -0700

    i am

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,9 @@
 #include "main.h"
 #include <string.h>
-int _strcmp(char *s1, char *s2)
+/*
+ * this funcation comper between
+ * two strings
+ */int _strcmp(char *s1, char *s2)
 {
 int i = 0;
 while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
