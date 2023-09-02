e8cfe019bb0fa9f8aa3ac8fe12146fd4bc834f37
amr89mohamed <amr89mohamed@gmail.com>
2023-09-02 08:13:16 -0700

    all is good till now

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+char *_strcpy(char *dest, char *src);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 int _putchar(char c);
+void puts2(char *str);
 #endif
