437f92e94ffec4e1a7fd29fd0d2c547946f818cf
amr89mohamed <amr89mohamed@gmail.com>
2023-09-02 08:56:35 -0700

    allis done

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+unsigned int _strspn(char *s, char *accept);
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
