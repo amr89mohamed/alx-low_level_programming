f9dbd2921e6c85b9b0e10dce5cb978fcaf210b12
amr89mohamed <amr89mohamed@gmail.com>
2023-09-06 14:52:10 -0700

    its imused

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+ * this funcation will return
+ *  prints the number of arguments passed into it.
+*/int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
