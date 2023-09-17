84aec39176a4a9a45d01313b609a60966c27c304
amr89mohamed <amr89mohamed@gmail.com>
2023-09-17 02:44:03 -0700

    all4

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,8 +7,8 @@
 {
 if (d)
 {
-printf("name:%s\n", d->name ? d->name : "(nil)");
-printf("age:%f\n", d->age);
-printf("owner:%s\n", d->owner ? d->owner : "(nil)");
+printf("name: %s\n", d->name ? d->name : "(nil)");
+printf("age: %f\n", d->age);
+printf("owner: %s\n", d->owner ? d->owner : "(nil)");
 }
 }
