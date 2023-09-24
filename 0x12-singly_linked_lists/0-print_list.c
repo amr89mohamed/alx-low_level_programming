a355b017f957af4060e3fbf7472a1c4f1201e581
amr89mohamed <amr89mohamed@gmail.com>
2023-09-24 05:49:50 -0700

    all is great3

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,15 +7,15 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t k = 0;
+size_t node = 0;
 while (h)
 {
 if (h->str == NULL)
 printf("[0] (nil)\n");
 else
 printf("[%d] %s\n", h->len, h->str);
-k++;
+node++;
 h = h->next;
 }
-return (k);
+return (node);
 }
