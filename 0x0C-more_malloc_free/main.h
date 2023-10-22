1500253ac3f7c81fd88fd30f5bffc1dfa09844a1
amr89mohamed <50431041+amr89mohamed@users.noreply.github.com>
2023-10-22 10:06:52 +0300

    Delete 0x0C-more_malloc_free directory

--- 0x0C-more_malloc_free/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdlib.h>
-void *malloc_checked(unsigned int b);
-int _putchar(char c);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-char *_memset(char *s, char b, unsigned int n);
-#endif
