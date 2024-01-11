3121691cb2c3cde81a5a1824157061db10817778
amr89mohamed <50431041+amr89mohamed@users.noreply.github.com>
2024-01-11 02:49:23 +0200

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/0-object_like_macro.h
+++
@@ -1,4 +0,0 @@
-#ifndef OBJECT_lIKE_MACRO_H
-#define OBJECT_lIKE_MACRO_H
-#define SIZE 1024
-#endif
--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,4 +0,0 @@
-#ifndef FUNCATION_LIKE_MACRO_H
-#define FUNCATION_LIKE_MACRO_H
-#define ABS(X) (((X) < (0)) ? ((X) * (-1)) : (X))
-#endif
