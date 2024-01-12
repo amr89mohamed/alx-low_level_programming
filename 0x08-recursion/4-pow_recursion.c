be3cf1b7a8be034036f32641c4bb9a68e9e731c1
amr89mohamed <amr89mohamed@gmail.com>
2024-01-11 19:02:00 -0800

    all is new

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * this funcation
+ * will return
+ * the power rised of n by y
+*/int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+if (y == 0)
+{
+return (1);
+}
+return (x * _pow_recursion(x, y - 1));
+}
