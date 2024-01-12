fb6d5cedeccf480aedaae6e9e0b55fbde349e1c1
amr89mohamed <amr89mohamed@gmail.com>
2024-01-11 17:48:04 -0800

    clean betty

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,15 @@
 #include "main.h"
-void _print_rev_recursion(char *s)
+/*
+ * this funcation
+ * will return the text
+ * reversed
+ * to the std/out
+ */void _print_rev_recursion(char *s)
 {
-  if (*s =='\0')
-    {
-      return;
-    }
-      _print_rev_recursion(s + 1);
-       _putchar(*s);
-       
+if (*s == '\0')
+{
+return;
+}
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
