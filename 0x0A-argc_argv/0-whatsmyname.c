fdf64972ce4fa84d69fba3b274ac3c1505ae6550
amr89mohamed <amr89mohamed@gmail.com>
2024-01-12 18:37:50 -0800

    the app will return the name of it

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * this funcation
+ * where is the main fancation
+ * takes two arguments
+ * the first is argc the number of argumants
+ * the Second is the argumants ve
+ */int main(int argc, char *argv[])
+{
+if (argc > 0)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
