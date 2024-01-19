d6711be20d8de0e6640afa241ac8b0597d67f5a5
amr89mohamed <amr89mohamed@gmail.com>
2024-01-18 18:38:38 -0800

    v1

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+void free_grid(int **grid, int height)
+{
+int i,j;
+for (i = 0; i < height; i++)
+{
+if (grid[i] == NULL)
+{
+for (j = 0; j < i; j++)
+free(grid[j]);
+free(grid);
+}
+}
+return;
+}
