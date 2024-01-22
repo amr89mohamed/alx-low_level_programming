79cc8ba732f84a928b5d842eb869209542d3b53b
amr89mohamed <amr89mohamed@gmail.com>
2024-01-21 20:53:38 -0800

    V1 of print_dog program

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "dog.h"
+/*
+ * this the print_dog funcation
+ * take @stract dog pointer as argument
+ * and return nothing
+*/void print_dog(struct dog *d)
+{
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", (*d).age);
+printf("Owner: %s\n", d->owner);
+}
