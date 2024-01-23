d23695351ea91041c3811c926dd70898d6af5800
amr89mohamed <amr89mohamed@gmail.com>
2024-01-22 18:40:44 -0800

    v4 of print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,8 +9,8 @@
 {
 if (d != NULL)
 {
-printf("Name: %s\n", d->name);
-printf("Age: %f\n", d->age);
-printf("Owner: %s\n", d->owner);
+printf("Name: %s\n", (d->name) ? d->name : "nil");
+printf("Age: %f\n", (d->age) ? d->age : 0);
+printf("Owner: %s\n", (d->owner) ? d->owner : "nil");
 }
 }
