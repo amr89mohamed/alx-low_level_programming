f751f78c77f8a4f172b28bb92afbe5029bf183e9
amr89mohamed <amr89mohamed@gmail.com>
2024-01-22 18:31:36 -0800

    update init && print app

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,7 +6,10 @@
  * and return nothing
 */void print_dog(struct dog *d)
 {
+if (d != NULL)
+{
 printf("Name: %s\n", d->name);
-printf("Age: %f\n", (*d).age);
+printf("Age: %f\n", d->age);
 printf("Owner: %s\n", d->owner);
+}
 }
