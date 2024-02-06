1b56bc5a1e2965138ae52999102cfd47f0a0f41c
amr89mohamed <amr89mohamed@gmail.com>
2024-02-05 21:45:26 -0800

    the get n node return the corect output

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/*
+ * this funcation will return a spacfic
+ * node to the main fun, using @count to spacify
+ * the node index number
+*/listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int count = 0;
+listint_t *ptr;
+ptr = head;
+if (head == NULL)
+{
+return (NULL);
+}
+ptr = head;
+while (count < index)
+{
+count++;
+ptr = ptr->next;
+}
+return (ptr);
+}
