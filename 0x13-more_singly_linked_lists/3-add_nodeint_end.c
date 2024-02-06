205cd29b1ad04930148bf98794d9545f56edb2ff
amr89mohamed <amr89mohamed@gmail.com>
2024-02-05 16:51:35 -0800

    create and add node to the end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,24 +1,30 @@
 #include "lists.h"
-listint_t *add_nodeint_end(listint_t **head, const int n)
+/*
+ * This fun return @head
+ * if the @head == NULL
+ * assign @temp address to pointer head
+ * and travirs pointer till the last node
+ * then add the new node address to the last node
+*/listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-  listint_t *temp = malloc(sizeof(listint_t));
-  listint_t *ptr;
-  if (temp == NULL)
-    {
-      return (NULL);
-    }
-   temp->n = n;
-  temp->next = NULL;
-  if (*head == NULL)
-    {
-      *head = temp;
-      return (*head);
-    }
-  ptr = *head;
-  while (ptr != NULL)
-    {
-      ptr = ptr->next;
-    }
-  ptr->next = temp;
-  return (*head);
+listint_t *temp = malloc(sizeof(listint_t));
+listint_t *ptr;
+if (temp == NULL)
+{
+return (NULL);
+}
+temp->n = n;
+temp->next = NULL;
+if (*head == NULL)
+*head = temp;
+else
+{
+ptr = *head;
+while (ptr->next != NULL)
+{
+ptr = ptr->next;
+}
+ptr->next = temp;
+}
+return (*head);
 }
