9f6327d7941b14cd15f546393ed97ac5b9309551
amr89mohamed <amr89mohamed@gmail.com>
2024-02-05 21:48:10 -0800

    v2 of git app

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -6,17 +6,14 @@
 */listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int count = 0;
-listint_t *ptr;
-ptr = head;
 if (head == NULL)
 {
 return (NULL);
 }
-ptr = head;
 while (count < index)
 {
 count++;
-ptr = ptr->next;
+head = head->next;
 }
-return (ptr);
+return (head);
 }
